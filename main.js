29b38a4e7833c2366fb250294c8c8ac005a4f5d8
Armand Zangue <armand.zangue@gmail.com>
2016-03-06 23:58:49 +0100

    add parse() to main

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
+import DashParser from "./src/dash/DashParser.js";
 import Yoda from "./src/Yoda.js";
 
 let mpdUrl = "http://dash.edgesuite.net/envivio/Envivio-dash2/manifest.mpd";
 let player = new Yoda();
+let parser = new DashParser();
 
 console.log("Creating the player...");
 player.loadManifest(mpdUrl);
@@ -9,4 +11,5 @@ player.loadManifest(mpdUrl);
 setTimeout(function() {
     let mpd = player.getManifest();
     console.log(mpd);
+    parser.parse(mpd);
 }, 2000);
