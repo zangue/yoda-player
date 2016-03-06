36fd152b0d245e20b902ab88e05206ea9c356670
Armand Zangue <armand.zangue@gmail.com>
2016-03-06 23:58:13 +0100

    start implementation of manifest parser

--- src/dash/DashParser.js
+++ src/dash/DashParser.js
@@ -1,7 +1,111 @@
-function DashParser () {
+import MPD from "./mpd/MPD.js";
+import Period from "./mpd/Period.js";
+import AdaptationSet from "./mpd/AdaptationSet.js";
+import Representation from "./mpd/Representation.js";
 
-    function parse (mpd) {
-        
+class DashParser {
+
+    parseAttribute (node, attr) {
+        return node.getAttribute(attr);
+    }
+
+    parseRepresentation (node) {
+
+    }
+
+    parseAdaptationSet (node) {
+        let as = new AdaptationSet();
+
+        as.xlinkHref = this.parseAttribute(node, "xlink:href");
+        as.xlinkActuate = this.parseAttribute(node, "xlink:actuate");
+        as.id = this.parseAttribute(node, "id");
+        as.group = this.parseAttribute(node, "group");
+        as.lang = this.parseAttribute(node, "lang");
+        as.contentType = this.parseAttribute(node, "contentType");
+        as.par = this.parseAttribute(node, "par");
+        as.minBandwidth = this.parseAttribute(node, "minBandwidth");
+        as.maxBandwidth = this.parseAttribute(node, "maxBandwidth");
+        as.minWidth = this.parseAttribute(node, "minWidth");
+        as.maxWidth = this.parseAttribute(node, "maxWidth");
+        as.minHeight = this.parseAttribute(node, "minHeight");
+        as.maxHeight = this.parseAttribute(node, "maxHeight");
+        as.minFrameRate = this.parseAttribute(node, "minFrameRate");
+        as.maxFrameRate = this.parseAttribute(node, "maxFrameRate");
+        as.segmentAlignment = this.parseAttribute(node, "segmentAlignment");
+        as.bitStreamSwitching = this.parseAttribute(node, "bitStreamSwitching");
+        as.subsegmentAlignment = this.parseAttribute(node, "subsegmentAlignment");
+        as.subsegmentStartsWithSAP = this.parseAttribute(node, "subsegmentStartsWithSAP");
+
+        return as;
+    }
+
+    parsePeriod (node) {
+        let period = new Period(),
+            adaptationSets;
+
+        period.xlinkHref = this.parseAttribute(node, "xlink:href");
+        period.xlinkActuate = this.parseAttribute(node, "xlink:actuate") || "onRequest";
+        period.id = this.parseAttribute(node, "id");
+        period.start = this.parseAttribute(node, "start");
+        period.duration = this.parseAttribute(node, "duration");
+        period.bitStreamSwitching = this.parseAttribute(node, "bitStreamSwitching");
+
+        adaptationSets = node.children;
+
+        for (let i = 0; i < adaptationSets.length; i++) {
+            period.adaptationSets[i] = this.parseAdaptationSet(adaptationSets[i]);
+        }
+
+        return period;
+    }
+
+    parseMPD (node) {
+        let mpd = new MPD(),
+            periods;
+
+        mpd.id = this.parseAttribute(node, "id");
+        mpd.profiles = this.parseAttribute(node, "profiles");
+        mpd.type = this.parseAttribute(node, "type") || "static";
+        mpd.availabilityStartTime = this.parseAttribute(node, "availabilityStartTime");
+        mpd.publishTime = this.parseAttribute(node, "publishTime");
+        mpd.availabilityEndTime = this.parseAttribute(node, "availabilityEndTime");
+        mpd.mediaPresentationDuration = this.parseAttribute(node, "mediaPresentationDuration");
+        mpd.minimumUpdatePeriod = this.parseAttribute(node, "minimumUpdatePeriod");
+        mpd.minBufferTime = this.parseAttribute(node, "minBufferTime");
+        mpd.timeShiftBufferDepth = this.parseAttribute(node, "timeShiftBufferDepth");
+        mpd.suggestedPresentationDelay = this.parseAttribute(node, "suggestedPresentationDelay");
+        mpd.maxSegmentDuration = this.parseAttribute(node, "maxSegmentDuration");
+        mpd.maxSubSegmentDuration = this.parseAttribute(node, "maxSubSegmentDuration");
+
+        periods = node.children;
+
+        for (let i = 0; i < periods.length; i++) {
+            //console.log(periods[i]);
+            mpd.periods[i] = this.parsePeriod(periods[i]);
+        }
+        console.dir(mpd);
+
+        return mpd;
+    }
+
+    parse (mpd) {
+        let parser = new DOMParser(),
+            xml = parser.parseFromString(mpd, "text/xml"),
+            mpdNode,
+            mpdObj;
+
+        if (!xml) {
+            console.error("Failed to parse Manifest file");
+            return null;
+        }
+
+        console.log("Parsing MPD...");
+
+        mpdNode = xml.getElementsByTagName("MPD")[0];
+
+        mpdObj = this.parseMPD(mpdNode);
+
+        return mpdObj;
     }
 
 }
