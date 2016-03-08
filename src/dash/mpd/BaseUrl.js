5d78f3c8eb1a702560e86d90f2c84ca7e3b7e60f
Armand Zangue <armand.zangue@gmail.com>
2016-03-08 16:06:39 +0100

    add BaseUrl object

---
+++ src/dash/mpd/BaseUrl.js
@@ -0,0 +1,8 @@
+class BaseUrl {
+    constructor () {
+        this.serviceLocation = null;
+        this.byteRange = null;
+        this.availabilityTimeOffset = null;
+        this.availabilityTimeComplete = null;
+    }
+}
