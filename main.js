863c48d1778f73fa5c5f071a71216d161d667819
Armand Zangue <armand.zangue@gmail.com>
2016-03-09 20:45:52 +0100

    test with different mpd

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 import DashParser from "./src/dash/DashParser.js";
 import Yoda from "./src/Yoda.js";
 
-let mpdUrl = "http://dash.edgesuite.net/envivio/Envivio-dash2/manifest.mpd";
+//let mpdUrl = "http://dash.edgesuite.net/envivio/Envivio-dash2/manifest.mpd";
+let mpdUrl = "http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys13/redbull_6sec.mpd";
 let player = new Yoda();
 let parser = new DashParser();
 
