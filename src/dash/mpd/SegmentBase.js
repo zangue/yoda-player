464e1dc12789d6f0fa4aab50f65db5a6eaff6474
Armand Zangue <armand.zangue@gmail.com>
2016-03-09 02:22:51 +0100

    adds attributes to SegmentBase

--- src/dash/mpd/SegmentBase.js
+++ src/dash/mpd/SegmentBase.js
@@ -3,7 +3,10 @@ class SegmentBase {
         this.timescale = null;
         this.presentationTimeOffset = null;
         this.timeShiftBufferDepth = null;
+        this.indexRange = null;
         this.indexRangeExact = false;
+        this.availabilityTimeOffset = null;
+        this.availabilityTimeComplete = null;
         this.initialization = null;
         this.representationIndex = null;
     }
