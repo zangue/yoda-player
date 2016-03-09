d47fc3716f153202b93e678b17c38b6c2d90ebab
Armand Zangue <armand.zangue@gmail.com>
2016-03-09 20:47:33 +0100

    adds generic segment object

---
+++ src/dash/mpd/Segment.js
@@ -0,0 +1,25 @@
+class Segment {
+    constructor () {
+        // Segment Base
+        this.timescale = null;
+        this.presentationTimeOffset = null;
+        this.timeShiftBufferDepth = null;
+        this.indexRange = null;
+        this.indexRangeExact = false;
+        this.availabilityTimeOffset = null;
+        this.availabilityTimeComplete = null;
+        this.initialization = null;
+        this.representationIndex = null;
+        // Segment Template
+        this.media = null;
+        this.index = null;
+        this.initialization = null;
+        this.bitstreamSwitching = null;
+        // Segment List
+        this.xlinkHref = null;
+        this.xlinkActuate = "onRequest";
+        this.segmentUrls = [];
+    }
+}
+
+export default Segment;
