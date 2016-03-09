83632d5045f327a8348c341a5ad8758e2c053205
Armand Zangue <armand.zangue@gmail.com>
2016-03-09 02:23:17 +0100

    adds attributes to SegmentList

--- src/dash/mpd/SegmentList.js
+++ src/dash/mpd/SegmentList.js
@@ -2,6 +2,10 @@ class SegmentList {
     constructor () {
         this.xlinkHref = null;
         this.xlinkActuate = "onRequest";
+        this.media = null;
+        this.mediaRange = null;
+        this.index = null;
+        this.indexRange = null;
         this.segmentUrls = [];
     }
 }
