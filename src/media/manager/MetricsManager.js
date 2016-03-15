14376572cf8bba4f08b8b8c7a381706f5f7cb20d
Armand Zangue <armand.zangue@gmail.com>
2016-03-15 22:32:29 +0100

    add first implementation of MetricsManager class

---
+++ src/media/manager/MetricsManager.js
@@ -0,0 +1,34 @@
+import HTTPRequest from "../metrics/HTTPRequest.js";
+import TCPConnection from "../metrics/TCPConnection.js";
+
+class MetricsManager {
+
+    constructor () {
+        this._TCPList = [];
+        this._HTTPList = [];
+    }
+
+    addHTTPRequest (tcpId, type, url, actualUrl, range, trequest, tresponse, tresponseCode, interval, traces, headers, latency) {
+        let http = new HTTPrequest();
+
+        http.tcpId = tcpId;
+        http.type = type;
+        http.url = url;
+        http.actualUrl = actualUrl;
+        http.range = range;
+        http.trequest = trequest;
+        http.tresponse = tresponse;
+        http.tresponseCode = tresponseCode;
+        http.interval = interval ? interval : 0;
+        http.traces = traces;
+        http.headers = headers;
+        http.latency = latency;
+
+        this._HTTPList.push(http);
+
+        return http;
+    }
+
+}
+
+export default MetricsManager;
