7690daab0fd352526420dc167993725f8cd1f13c
Armand Zangue <armand.zangue@gmail.com>
2016-03-15 14:15:26 +0100

    adds interface to get representation infos from manifest and get segment urls list for specifec representation

--- src/dash/DashDriver.js
+++ src/dash/DashDriver.js
@@ -11,12 +11,50 @@ class DashDriver {
         as = period.adaptationSets;
 
         for (i = 0; i < as.length; i++) {
-            asType = as[i].common.mimeType.split('/')[0];
+            if (as[i].common.mimeType) {
+                asType = as[i].common.mimeType.split('/')[0];
+            } else {
+                asType = as[i].representations[0].common.mimeType.split('/')[0];
+            }
 
             if (asType == type) {
                 return as[i];
             }
         }
+    }
+
+    getRepresentationForBitrate (mediaType, bitrate, manifest) {
+        let as = this._getAdaptationForType(mediaType, manifest),
+            reps = as.representations,
+            representation = null,
+            i;
+
+        for (i = 0; i < reps.length; i++) {
+            let repBitrate = parseInt(reps[i].bandwidth);
+
+            if (repBitrate === bitrate) {
+                representation = reps[i];
+                break;
+            }
+        }
+
+        return representation;
+    }
+
+    _getSegmentsFromTemplate (representation) {
+
+    }
+
+    _getSegmentsFromList (representation) {
+        let list = representation.segmentList[0].segmentUrls,
+            i,
+            segments = [];
+
+        for (i = 0; i < list.length; i++) {
+            segments.push(list[i].media);
+        }
+
+        return segments;
     }
 
     getMediaInfoFor (mediaType, manifest) {
@@ -35,6 +73,20 @@ class DashDriver {
         }
 
         return mediaInfo;
+    }
+
+    getSegments (representation) {
+        let segments;
+
+        if (representation.segmentList) {
+            segments = this._getSegmentsFromList(representation);
+        } else if (representation.segmentTemplate) {
+
+        } else if (representation.segmentBase) {
+
+        }
+
+        return segments;
     }
 
 
