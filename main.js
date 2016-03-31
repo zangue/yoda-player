79bf2fb0923702b7f72b0cb5c2670434626be769
Armand Zangue <armand.zangue@gmail.com>
2016-04-01 00:09:42 +0200

    start implementing startup logic in Yoda class. update main.js file to export Player

--- main.js
+++ main.js
@@ -1,16 +1,32 @@
-import DashParser from "./src/dash/DashParser.js";
 import Yoda from "./src/Yoda.js";
 
 //let mpdUrl = "http://dash.edgesuite.net/envivio/Envivio-dash2/manifest.mpd";
+/*
 let mpdUrl = "http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys13/redbull_6sec.mpd";
 let player = new Yoda();
 let parser = new DashParser();
+let manifestLoader = new ManifestLoader();
 
 console.log("Creating the player...");
-player.loadManifest(mpdUrl);
+manifestLoader.load(mpdUrl);
 
 setTimeout(function() {
-    let mpd = player.getManifest();
+    let mpd = manifestLoader.getManifest();
     console.log(mpd);
-    parser.parse(mpd);
+    let manifest = parser.parse(mpd);
+
+    player.startup(manifest);
 }, 2000);
+*/
+
+var context = window || global;
+
+var yoda = context.yoda;
+if (!yoda) {
+    yoda = context.yoda = {}
+}
+
+yoda.Player = Yoda;
+
+export default yoda;
+export {Yoda};
--- src/Yoda.js
+++ src/Yoda.js
@@ -1,45 +1,155 @@
 import DashParser from "./dash/DashParser.js";
 import DashDriver from "./dash/DashDriver.js";
+import MediaInfo from "./media/objects/infos/MediaInfo.js";
+import ManifestLoader from "./media/ManifestLoader.js";
+import EventBus from "./lib/EventBus.js";
+import Events from "./media/Events.js";
 
 class Yoda {
-    constructor () {
-        this.manifest = null;
+    constructor (config) {
+        this._config = config;
     }
 
-    loadManifest (url) {
-        let request = new XMLHttpRequest(),
-            onload,
-            onerror;
-
-        onload = function () {
-            if (request.status === 200) {
-                this.manifest = request.responseText;
-            }
-        };
-
-        onerror = function () {
-            console.error("Could not load manifest.");
-        };
-
-        console.log("Loading manifest...");
-
-        try {
-            request.onload = onload.bind(this);
-            request.onerror = onerror;
-
-            request.open("GET", url, true);
-            request.responseType = "text";
-            request.send();
-        } catch (e) {
-            request.onerror();
+    setup () {
+        if (!this._config.mpd) {
+            throw "No source set!";
         }
 
-        console.log("Loading manifest... DONE.");
+        if (!this._config.id) {
+            throw "Video element ID is missing!";
+        }
+
+        EventBus.subscribe(Events.MANIFEST_LOADED, this.onManifestLoaded, this);
+
+        let manifestLoader = new ManifestLoader();
+
+        manifestLoader.load(this._config.mpd);
     }
 
-    getManifest() {
-        return this.manifest;
+    onManifestLoaded (event) {
+        if (!event.manifest) {
+            console.log("onManifestLoaded: Could not load manifest file");
+            throw "Could not load manifest file";
+        }
+
+        console.log("Manifest Loaded");
+        console.log(event.manifest);
+
+        let parser = new DashParser();
     }
+
+    reset () {
+        EventBus.unsubscribe(Events.MANIFEST_LOADED, this.onManifestLoaded, this);
+
+        this.setup();
+    }
+
 }
 
+/*
+class Yoda {
+    constructor () {
+        // set mimetype and codecs
+        this.mimeType = "video/mp4";
+        this.codecs = "avc1.42c01e";
+        this.amimeType = "audio/mp4";
+        this.acodecs = "mp4a.40.2";
+
+        // create media source instance
+        this.ms = new MediaSource();
+        this.vsourceBuffer = null;
+        this.asourceBuffer = null;
+        this.segNum = 1;
+        this.asegNum = 1;
+        this.maxSegNum = 875;
+        this.video = null;
+        this.bufferCount = 0;
+
+        this.dash = new DashDriver();
+    }
+
+    startup (manifest) {
+        console.log("Starting up ...");
+        let mediaInfo = this.dash.getMediaInfoFor("video", manifest);
+        let audioInfo = this.dash.getMediaInfoFor("audio", manifest);
+        let rep = this.dash.getRepresentationForBitrate("video", mediaInfo.bitrates[0], manifest);
+        let urlList = this.dash.getSegments(rep);
+
+        console.dir(mediaInfo);
+        console.dir(audioInfo);
+        console.dir(urlList);
+        // add event listeners
+        this.ms.addEventListener("webkitsourceopen", this.onSourceOpened.bind(this), false);
+        this.ms.addEventListener("webkitsourceclose", this.onSourceClosed.bind(this), false);
+        this.ms.addEventListener("sourceopen", this.onSourceOpened.bind(this), false);
+
+        // get reference to video
+        this.video = document.querySelector("video");
+
+        // set mediasource as source for the video
+        this.video.src = window.URL.createObjectURL(this.ms);
+        this.video.width = 1280;
+        this.video.height = 720;
+    }
+
+    onSourceOpened () {
+        // create source buffer
+        let value = this.mimeType + ';codecs="' + this.codecs + '"',
+            avalue = this.amimeType + ';codecs="' + this.acodecs + '"',
+            initSegmentURL = "http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys13/video/redbull_6sec/2000kbps/redbull_720p_2000kbps_6sec_segmentinit.mp4";
+            //initSegmentURL = "http://dash.edgesuite.net/envivio/Envivio-dash2/v3_257-Header.m4s";
+
+        console.log("Add Video buffer");
+        this.vsourceBuffer = this.ms.addSourceBuffer(value);
+        console.log("MediaSource readystate: " + this.ms.readyState);
+
+        // request initialization segment
+        let request = new XMLHttpRequest();
+        request.responseType = "arraybuffer";
+        request.open("GET", initSegmentURL, true);
+        request.onload = (function () {
+            let array = new Uint8Array(request.response);
+
+            this.vsourceBuffer.appendBuffer(array);
+
+            // wait until the sourceBuffer is primed with the initialization
+            // before loading contentType
+            this.vsourceBuffer.addEventListener("updateend", this.loadSegment.bind(this));
+        }).bind(this);
+        request.send();
+    }
+
+    onSourceClosed () {
+
+    }
+
+    loadSegment () {
+        //if (Math.abs(this.asegNum - this.segNum) > 2) return;
+        if (this.segNum < this.maxSegNum) {
+            this.getSegment();
+            this.segNum++;
+        }
+    }
+
+    getSegment () {
+        let request = new XMLHttpRequest(),
+            segmentURL = "http://www-itec.uni-klu.ac.at/ftp/datasets/mmsys13/video/redbull_6sec/2000kbps/redbull_720p_2000kbps_6sec_segment" + this.segNum + ".m4s";
+            //segmentURL = "http://dash.edgesuite.net/envivio/Envivio-dash2/v3_257-270146-i-" + this.segNum + ".m4s";
+
+
+        console.log("Getting segment: " + segmentURL);
+        request.responseType = "arraybuffer";
+        request.open("GET", segmentURL, true);
+        request.onload = (function () {
+            let array = new Uint8Array(request.response);
+            this.vsourceBuffer.appendBuffer(array);
+        }).bind(this);
+        request.send();
+    }
+
+    play () {
+
+    }
+}
+*/
 export default Yoda;
