520cdcaed16387454b306e3724efeacc8663bf3b
Armand Zangue <armand.zangue@gmail.com>
2016-04-03 22:57:07 +0200

    add generic Request object

---
+++ src/media/objects/Request.js
@@ -0,0 +1,14 @@
+class Request {
+    constructor () {
+        this.mediaType = null;
+        this.range = null;
+        this.url = null;
+        this.bitrate = null;
+        this.index = NaN;
+        this.responseType = null;
+        this.method = null;
+        this.async = null;
+    }
+}
+
+export default Request;
