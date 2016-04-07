01fc5c716af23785a4285b6ecd9c19e6737820c8
Armand Zangue <armand.zangue@gmail.com>
2016-04-07 20:09:45 +0200

    made fragment loader use XHR class to download segments

--- src/media/FragmentLoader.js
+++ src/media/FragmentLoader.js
@@ -1,115 +1,62 @@
-import ThroughputTrace from "./metrics/ThroughputTrace.js";
-import Fragment from "./objects/Fragment";
+import Fragment from "./objects/Fragment.js";
+import EventBus from "../lib/EventBus.js";
+import Events from "../media/Events.js";
+import XHR from "./net/XHR.js";
 
+// TODO
 class FragmentLoader {
 
-    constructor (metricsManager) {
-        this._metricsManager = metricsManager;
+    constructor () {
     }
 
     load (request) {
-        let xhr = new XMLHttpRequest(),
-            isFistProgress = true,
-            requestStartDate = null,
-            firstByteDate = null,
-            lastTraceDate = null,
-            lastLoadedCount = 0,
-            interval = 0,
-            traces = [],
-            addMetrics,
-            onload,
-            onloadstart,
-            onprogress,
-            onerror;
+        let xhr,
+            callbacks = {};
 
-        addMetrics = function () {
-            let now = new Date(),
-                requestEndDate = now,
-                headers = xhr.getAllResponseHeaders(),
-                latency;
+        callbacks.onload = function (xhr) {
+            if (xhr.status >= 200 && xhr.status <= 299) {
+                console.log("Fragment Loaded success");
+                //console.dir(xhr);
+                let fragment = new Fragment();
 
-            latency = firstByteDate.getTime() - requestStartDate.getTime();
+                fragment.mediaType = request.mediaType;
+                fragment.dataChunk = xhr.response;
+                fragment.init = request.init;
 
-            this._metricsManager.addHTTPRequest(
-                null, // tcpId
-                request.mediaType, // type
-                request.url, // url
-                request.url, // actualUrl
-                request.range, // range
-                requestStartDate, // trequest
-                requestEndDate, // tresponse
-                xhr.status, // tresponseCode
-                interval, // interval
-                traces, // traces
-                headers, // headers
-                latency // latency
+                EventBus.broadcast(
+                    Events.FRAGMENT_LOADED, {
+                        fragment: fragment
+                    }
+                );
+            }
+        };
+
+        callbacks.onprogress = function (xhr, trace) {
+            console.log("Fragment Loading progress ...");
+            EventBus.broadcast(
+                Events.FRAGMENT_PROGRESS, {
+                    trace: trace
+                }
             );
         };
 
-        onloadstart = function () {
-            requestStartDate = lastTraceDate = new Date();
-        };
-
-        onload = function () {
-            let fragment;
-
-            if (request.status < 200 || request.status > 299) {
-                return null;
-            }
-
-            fragment = new Fragment();
-
-            fragment.mediaType = request.mediaType;
-            fragment.dataChunk = new Uint8Array(request.response);
-
-            // TODO - Fire event FRAGMENT_LOADED and loaded fragment available
-
-            addMetrics();
-
-            console.log("Loaded " + request.mediaType);
-        };
-
-        onprogress = function (event) {
-            let now = new Date(),
-                trace = new ThroughputTrace();
-
-            if (isFistProgress) {
-                isFistProgress = false;
-
-                if (!event.lengthComputable || (event.lengthComputable && event.total !== event.loaded)) {
-                    firstByteDate = now;
+        callbacks.onerror = function (xhr) {
+            console.log("Error");
+            EventBus.broadcast(
+                Events.FRAGMENT_LOADED, {
+                    fragment: null
                 }
-            }
-
-            trace.start = lastTraceDate;
-            trace.duration = now.getTime() - lastTraceDate.getTime();
-            trace.bytes = [event.loaded ? event.loaded - lastLoadedCount : 0];
-
-            traces.push(trace);
-
-            interval += trace.duration;
-            lastTraceDate = now;
-            lastLoadedCount = event.loaded;
+            );
         };
 
-        onerror = function () {
-            addMetrics();
+        console.log("Loading Fragment...");
 
-            console.log("Failed " + request.mediaType);
-        };
+        xhr = new XHR();
 
         try {
-            xhr.onloadstart = onloadstart.bind(this);
-            xhr.onload = onload.bind(this);
-            xhr.onprogress = onprogress.bind(this);
-            xhr.onerror = onerror.bind(this);
-
-            xhr.open("GET", request.url, true);
-            xhr.responseType = request.responseType;
-
-            xhr.send();
+            xhr.load(request, callbacks);
         } catch (e) {
-            console.log("Something went wrong during fragment request: " + e.message);
+            console.log("[FragmentLoader] Something went wrong during http request: " + e.message);
         }
     }
 
