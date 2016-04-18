f39bac2c011c7ab7a66e78e99b47462854e6d7d8
Armand Zangue <armand.zangue@gmail.com>
2016-04-18 13:37:12 +0200

    add event subscriptions and new constructor variable

--- src/media/StreamEngine.js
+++ src/media/StreamEngine.js
@@ -8,9 +8,10 @@ import EventBus from "../lib/EventBus.js";
 import Events from "./Events.js";
 
 class StreamEngine {
-    constructor (mediaType, bufferManager) {
+    constructor (mediaType, bufferManager, videoElement) {
         this.mediaType = mediaType;
         this.bufferManager = bufferManager;
+        this.element = videoElement;
         this.fragmentManager = new FragmentManager();
         this.fragmentLoader = new FragmentLoader();
         this.mediaInfo = DashDriver.getMediaInfoFor(this.mediaType);
@@ -22,6 +23,13 @@ class StreamEngine {
     // suscribe event on fragment loaded
     setup () {
         EventBus.subscribe(Events.FRAGMENT_LOADED, this.onFragmentLoaded, this);
+        EventBus.subscribe(Events.PLAYBACK_PAUSED, this.onPlaybackPaused, this);
+        EventBus.subscribe(Events.PLAYBACK_RESUMED, this.onPlaybackResumed, this);
+        EventBus.subscribe(Events.PLAYBACK_SEEKING, this.onPlaybackSeeking, this);
+        EventBus.subscribe(Events.PLAYBACK_SEEKED, this.onPlaybackSeeked, this);
+        EventBus.subscribe(Events.PLAYBACK_STALLED, this.onPlaybackStalled, this);
+        EventBus.subscribe(Events.PLAYBACK_ENDED, this.onPlaybackEnded, this);
+        EventBus.subscribe(Events.PLAYBACK_CANPLAYTHROUGH, this.onPlaybackCanPlayThrough, this);
     }
 
     start () {
@@ -54,10 +62,16 @@ class StreamEngine {
     // See if we stopped because buffer were full and
     // schedule next segments if there some place now
     onFragmentAppended (data) {
-
+        scheduleNext();
     }
 
     onFragmentLoaded (data) {
+        if (!data.fragment)
+            return;
+
+        if (data.fragment.mediaType !== this.mediaType)
+            return;
+
         console.log("on Fragment Loaded!");
         //console.dir(data);
 
@@ -76,8 +90,43 @@ class StreamEngine {
         // request schedule of next fragment
     }
 
+    onPlaybackPaused (data) {
+
+    }
+
+    onPlaybackResumed (data) {
+
+    }
+
+    onPlaybackSeeking (data) {
+
+    }
+
+    onPlaybackSeeked (data) {
+
+    }
+
+    onPlaybackStalled (data) {
+
+    }
+
+    onPlaybackEnded (data) {
+
+    }
+
+    onPlaybackCanPlayThrough (data) {
+
+    }
+
     reset () {
         EventBus.unsubscribe(Events.FRAGMENT_LOADED, this.onFragmentLoaded, this);
+        EventBus.unsubscribe(Events.PLAYBACK_PAUSED, this.onPlaybackPaused, this);
+        EventBus.unsubscribe(Events.PLAYBACK_RESUMED, this.onPlaybackResumed, this);
+        EventBus.unsubscribe(Events.PLAYBACK_SEEKING, this.onPlaybackSeeking, this);
+        EventBus.unsubscribe(Events.PLAYBACK_SEEKED, this.onPlaybackSeeked, this);
+        EventBus.unsubscribe(Events.PLAYBACK_STALLED, this.onPlaybackStalled, this);
+        EventBus.unsubscribe(Events.PLAYBACK_ENDED, this.onPlaybackEnded, this);
+        EventBus.unsubscribe(Events.PLAYBACK_CANPLAYTHROUGH, this.onPlaybackCanPlayThrough, this);
         this.setup();
     }
 }
