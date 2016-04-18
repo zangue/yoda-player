b023c92fb5d5be2e97ad2b14432cdd52592d9db9
Armand Zangue <armand.zangue@gmail.com>
2016-04-18 13:38:40 +0200

    add event dispacth and fixed typo

--- src/media/VideoTag.js
+++ src/media/VideoTag.js
@@ -15,13 +15,19 @@ class VideoTag {
         this._mediaEvents = {
             "play": this.onPlay,
             "pause": this.onPause,
-            "seeking": this.onSeeking,
+            "seeking": this.onSeeking, // sent when the seeking operation begins
+            "seeked": this.onSeeked, // sent when the seeking operation stopped
             "stalled": this.onStalled,
-            "ended": this.onEnded
+            "ended": this.onEnded,
+            "canplaythrough": this.onCanPlayThrough
         };
     }
 
-    init () {
+    setup () {
+        // Set default dimensions
+        this._video.width = 640;
+        this._video.height = 360;
+
         for (let event in this._mediaEvents) {
             this._video.addEventListener(event, this._mediaEvents[event]);
         }
@@ -57,7 +63,7 @@ class VideoTag {
             this._video.duration = duration;
     }
 
-    getDuratioin () {
+    getDuration () {
         return this._video.duration;
     }
 
@@ -65,25 +71,84 @@ class VideoTag {
         return this._video.paused;
     }
 
+    getWidth () {
+        return this._video.width;
+    }
+
+    setWidth (value) {
+        this._video.width = value;
+    }
+
+    getHeight () {
+        return this._video.height;
+    }
+
+    setHeight (value) {
+        this._video.height = value;
+    }
+
     // Media Events
     onPlay () {
         console.log("on play");
+        EventBus.broadcast(
+            Events.PLAYBACK_RESUMED, {
+                data: null
+            }
+        );
     }
 
     onPause () {
         console.log("on pause");
+        EventBus.broadcast(
+            Events.PLAYBACK_PAUSED, {
+                data: null
+            }
+        );
     }
 
     onSeeking () {
         console.log("on seeking");
+        EventBus.broadcast(
+            Events.PLAYBACK_SEEKING, {
+                data: null
+            }
+        );
+    }
+
+    onSeeked () {
+        console.log("on seeked");
+        EventBus.broadcast(
+            Events.PLAYBACK_SEEKED, {
+                data: null
+            }
+        );
     }
 
     onStalled () {
         console.log("on stalled");
+        EventBus.broadcast(
+            Events.PLAYBACK_STALLED, {
+                data: null
+            }
+        );
     }
 
     onEnded () {
         console.log("on ended");
+        EventBus.broadcast(
+            Events.PLAYBACK_ENDED, {
+                data: null
+            }
+        );
+    }
+
+    onCanPlayThrough () {
+        console.log("on canplaythrough");
+        EventBus.broadcast(
+            Events.PLAYBACK_CANPLAYTHROUGH, {
+                data: null
+            }
+        );
     }
 }
 
