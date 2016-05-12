3bc793b72d78cd68050d506966fa38281dd6f0c9
Armand Zangue <armand.zangue@gmail.com>
2016-05-12 16:52:41 +0200

    refactor: rename virtualBuffer to buffer

--- src/media/manager/BufferManager.js
+++ src/media/manager/BufferManager.js
@@ -5,13 +5,14 @@ class BufferManager {
     constructor (mediaType, sourceBuffer) {
         this.mediaType = mediaType;
         this.sourceBuffer = sourceBuffer;
-        this.virtualBuffer = new Buffer();
+        this.buffer = new Buffer();
         this.manifestInfo = DashDriver.getManifestInfos();
     }
 
     // suscribe event
     setup() {
         this.sourceBuffer.addEventListener("updateend", this.onSourceBufferUpdateEnd.bind(this));
+        //console.dir(this.manifestInfo); debugger;
     }
 
     // append next
@@ -21,21 +22,21 @@ class BufferManager {
     }
 
     AppendToSource() {
-        this.virtualBuffer.append(this.sourceBuffer);
+        this.buffer.append(this.sourceBuffer);
     }
 
     canStartPlayback() {
         //console.log("maxSegmentDuration: " + this.manifestInfo.maxSegmentDuration);
         //console.log("minBufferTime: " + this.manifestInfo.minBufferTime);
         // TODO - check if we have minBufferTime.
-        if (this.virtualBuffer.getLength() > 2) {
+        if (this.buffer.getLength() > 4) {
             return true;
         }
 
         return false;
     }
 
-    // Check buffer level againt minBufferTime
+    // Check buffer level againt minBufferTime or suggestedMinBufferTime
     // get dash manifest information
     // decide
     shouldBuffer () {
@@ -45,10 +46,10 @@ class BufferManager {
     feedBuffer (fragment) {
         let wasDry = false;
 
-        if (this.virtualBuffer.getLength() === 0)
+        if (this.buffer.getLength() === 0)
             wasDry = true;
 
-        this.virtualBuffer.push(fragment);
+        this.buffer.push(fragment);
 
         if (wasDry)
             this.AppendToSource();
