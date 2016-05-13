5f2d521a5a3e47ef06d16b6c3def1c58fa894c51
Armand Zangue <armand.zangue@gmail.com>
2016-05-13 18:34:00 +0200

    add verbosity / add getHTTPList

--- src/media/manager/MetricsManager.js
+++ src/media/manager/MetricsManager.js
@@ -9,10 +9,20 @@ class MetricsManager {
     }
 
     addHTTPRequest (http) {
-        if (!(http instanceof HTTPRequest))
+        console.log("[MetricsManager] addHTTPRequest()");
+        //console.dir(http);
+        if (!(http instanceof HTTPRequest)) {
+            console.log("[MetricsManager] arg is not and HTTPRequest object. Skip adding");
             return;
+        }
 
         this._HTTPList.push(http);
+    }
+
+    getHTTPList () {
+        console.log("[metricsManager] getHTTPList()");
+        //console.dir(this._HTTPList);
+        return this._HTTPList;
     }
 }
 
